f3fea7c63c581e84397bb44f44e998a1691025c0
borisreyson <borisreyson@outlook.com>
2022-03-15 14:40:32 +0800

    - home.dart
    macbook boris

--- lib/screens/pages/home.dart
+++ lib/screens/pages/home.dart
@@ -110,7 +110,7 @@ class _HomePageState extends State<HomePage> {
         
         );
     print("Boris");
-    print("Jam Sekarang = ${mulai}");
+    print("Jam Sekarang jj= ${mulai}");
     NetworkCheck().checkConnection(context);
     setCustomMapPin();
     if (Platform.isAndroid) {
