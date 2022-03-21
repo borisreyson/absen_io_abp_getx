63b8468fe525c4155cbdaba645d162ae1cd4dff9
Hendra Rachman <hendrad8@gmail.com>
2022-03-21 16:34:31 +0800

    Update - detail_profile - buletin - screen menu - karyawan -divisi -departemen -roster kerja -roster cuti by Hendra

---
+++ lib/screens/pages/add_departemen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class AddDepartemen extends StatefulWidget {
+  const AddDepartemen({ Key? key }) : super(key: key);
+
+  @override
+  State<AddDepartemen> createState() => _AddDepartemenState();
+}
+
+class _AddDepartemenState extends State<AddDepartemen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xffffffff),
+         elevation: 0,
+         leading: InkWell(
+         splashColor: const Color(0xff000000),
+         child: const Icon(
+          Icons.arrow_back_ios_new,
+          color: Color(0xff000000),
+        ),
+        onTap: () {
+          Navigator.maybePop(context);
+        },
+      ),
+      title: const Text(
+        "Tambah Departemen",
+        style: TextStyle(color: Colors.black),
+      ),
+      ),
+
+    body: Container(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,  
+                  labelText: "ID Departemen",                      
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,  
+                  labelText: "Departemen",
+                  hintText: "Masukkan nama departemen"                      
+                ),
+              ),
+            ),
+
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 19, right: 19, top: 5),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    ),
+                    onPressed: (){
+              
+                    }, child: Text("Simpan",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),), 
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    ),  
+    );
+  }
+}
---
+++ lib/screens/pages/add_karyawan.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+
+class FormKaryawan extends StatefulWidget {
+  const FormKaryawan({ Key? key }) : super(key: key);
+
+  @override
+  State<FormKaryawan> createState() => _FormKaryawanState();
+}
+
+class _FormKaryawanState extends State<FormKaryawan> {
+
+  final _scafoldKey = GlobalKey<ScaffoldState>();
+  final _formKey = GlobalKey<FormState>();
+
+  String? _departemen;
+  String? _divisi;
+  String? _perusahaan;
+
+  List <String> itemDepartemen = [
+    '--Pilih Departemen--',
+    'HCGA',
+    'HSE',
+  ];
+
+  List <String> itemDivisi = [
+    '--Pilih Jabatan--',
+    'IT',
+    'Admin',
+    'GL'
+  ];
+
+  List <String> itemPerusahaan = [
+    '--Pilih Perusahaan--',
+    'MTK',
+    'PT.ABP',
+    'MNK'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(key: _scafoldKey,
+      appBar: AppBar(
+        backgroundColor: const Color(0xffffffff),
+         elevation: 0,
+         leading: InkWell(
+         splashColor: const Color(0xff000000),
+         child: const Icon(
+          Icons.arrow_back_ios_new,
+          color: Color(0xff000000),
+        ),
+        onTap: () {
+          Navigator.maybePop(context);
+        },
+      ),
+      title: const Text(
+        "Tambah Karyawan",
+        style: TextStyle(color: Colors.black),
+      ),
+      ),
+
+    body: Form( key: _formKey,
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10, left: 8, right: 8),
+            child: Column(
+              children: <Widget> [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,  
+                    labelText: "NIK",
+                    hintText: "Masukkan NIK Karyawan"                       
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,  
+                    labelText: "Nama",
+                    hintText: "Masukkan Nama Karyawan"                       
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    child: DropdownButtonFormField<String>(
+                      value: _departemen, style: TextStyle(color: Colors.black),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                      labelText: "Departemen",
+                      floatingLabelBehavior: FloatingLabelBehavior.always
+                      ),
+                      items: itemDepartemen.map<DropdownMenuItem<String>> ((String a){
+                          return DropdownMenuItem<String> (
+                            value: a,
+                            child: Text(a),
+                          );
+                        }).toList(),
+                        hint: Text("--Pilih Departemen--"),
+                        onChanged: (String? b){
+                          setState(() {
+                            _departemen = b; 
+                          });
+                        
+                        }),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    child: DropdownButtonFormField<String>(
+                      value: _divisi, style: TextStyle(color: Colors.black),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                      labelText: "Divisi",
+                      floatingLabelBehavior: FloatingLabelBehavior.always
+                      ),
+                      items: itemDivisi.map<DropdownMenuItem<String>> ((String a){
+                          return DropdownMenuItem<String> (
+                            value: a,
+                            child: Text(a),
+                          );
+                        }).toList(),
+                        hint: Text("--Pilih Divisi--"),
+                        onChanged: (String? b){
+                          setState(() {
+                            _divisi = b; 
+                          });
+                        
+                        }),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,  
+                    labelText: "Jabatan",
+                    hintText: "Masukkan Jabatan Karyawan"                       
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    child: DropdownButtonFormField<String>(
+                      value: _perusahaan, style: TextStyle(color: Colors.black),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                      labelText: "Perusahaan",
+                      floatingLabelBehavior: FloatingLabelBehavior.always
+                      ),
+                      items: itemPerusahaan.map<DropdownMenuItem<String>> ((String a){
+                          return DropdownMenuItem<String> (
+                            value: a,
+                            child: Text(a),
+                          );
+                        }).toList(),
+                        hint: Text("--Pilih Perusahaan--"),
+                        onChanged: (String? b){
+                          setState(() {
+                            _perusahaan = b; 
+                          });
+                        
+                        }),
+                  ),
+                ),
+
+                Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 19, right: 19, top: 5),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                            ),
+                            onPressed: (){
+              
+                            }, child: Text("Simpan",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),), 
+                          ),
+                        ),
+                      ),
+                    )
+              ],
+            ),
+          )
+        ],
+      )
+      ),  
+    );
+  }
+}
